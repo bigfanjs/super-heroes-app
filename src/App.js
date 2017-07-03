b498d3f5eb6da2fbcc73e6cbf20372f3ae32e8c8
Adel Jojo <bigfanjs@gmail.com>
2017-07-04 00:07:57 +0100

    feat: creates two lazily loaded routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React from "react";
+import {BrowserRouter as Router, Route, Redirect} from "react-router-dom";
+
+import asyncComponent from "./components/async-component";
+
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const
+  SuperHero = asyncComponent(() => import("./scenes/super-hero")),
+  SuperHeroList = asyncComponent(() => import("./scenes/super-hero-list"));
 
-export default App;
+export default function () {
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" render={() => <Redirect to="/heros" />} />
+        <Route exact path="/heros" component={SuperHeroList} />
+        <Route path="/heros/view/:id" component={SuperHero} />
+      </div>
+    </Router>
+  );
+}
