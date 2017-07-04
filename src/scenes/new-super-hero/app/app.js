97b26563e6367d0722e9f51c82890eabf7a1c659
Adel Jojo <bigfanjs@gmail.com>
2017-07-05 00:06:46 +0100

    feat: connects the new super hero component to the store and creates a wrapper around it

--- src/scenes/new-super-hero/app/app.js
+++ src/scenes/new-super-hero/app/app.js
@@ -1,16 +1,37 @@
 import React from "react";
+import {connect} from "react-redux";
 
+import {AddHero} from "../../../actions";
 import Input from "../input-field";
 
 import "./app.css";
 
-export default function () {
-  return (
-    <form className="new-super-hero">
-      <Input label="Real Name" />
-      <Input label="Nick Name" />
-      <Input type="submit" value="Save" />
-      <Input type="reset" value="Clear" />
-    </form>
-  );
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="new-super-hero" >
+        <form onSubmit={this.handleSubmit}>
+          <Input name="realname" label="Real Name" />
+          <Input name="nickname" label="Nick Name" />
+          <Input type="submit" value="Save" />
+          <Input type="reset" value="Clear" />
+        </form>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = function ({ formData }) {
+  return { values: formData };
+};
+
+export default connect(mapStateToProps)(Form);
