1a09b092d95bad283c5c1943de371b3892e8ea6a
Adel Jojo <bigfanjs@gmail.com>
2017-07-05 00:13:03 +0100

    feat: connects the new-super-hero's input-field sub-component to the store

--- src/scenes/new-super-hero/input-field/input-field.js
+++ src/scenes/new-super-hero/input-field/input-field.js
@@ -1,14 +1,41 @@
 import React from "react";
+import {connect} from "react-redux";
 
-export default function ({type="text", label, value}) {
-  if (!label) {
-    return <input type={type} value={value} />;
+import {updateValue} from "../../../actions";
+
+class Field extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  return (
-    <label>
-      { label }:
-      <input type={type} />
-    </label>
-  );
+  handleChange(e) {
+    this.props.updateValue(this.props.name, e.target.value);
+  }
+
+  render() {
+    const {label, value, type} = this.props;
+
+    if (!label) {
+      return <input type={type} value={value} />;
+    }
+
+    return (
+      <label>
+        { label }:
+        <input type={type} onChange={this.handleChange} />
+      </label>
+    );
+  }
 }
+
+const mapDispatchToProps = function (dispatch) {
+  return {
+    updateValue(name, value) {
+      dispatch(updateValue(name, value));
+    }
+  };
+};
+
+export default connect(undefined, mapDispatchToProps)(Field);
