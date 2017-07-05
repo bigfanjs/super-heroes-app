6723b2a89173592425f3a3f1307d7667eb3dea90
Adel Jojo <bigfanjs@gmail.com>
2017-07-05 13:40:55 +0100

    feat: updates and renames the form-data reducer to just form

---
+++ src/reducers/form.js
@@ -0,0 +1,20 @@
+const initialState = {
+  isLoading: false,
+  errors: {},
+  values: {}
+};
+const assign = Object.assign;
+
+export default function (state=initialState, action) {
+  switch (action.type) {
+    case "FORM_UPDATE_VALUE":
+      return assign({}, state, {
+        values: assign({}, state.values, {[action.name]: action.value})
+      }
+      );
+    case "FORM_RESET":
+      return state;
+    default:
+      return state;
+  }
+}
