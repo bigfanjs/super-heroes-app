949cd85b6fa6e40d49dd93b7a7a9457bc2c3149a
Adel Jojo <bigfanjs@gmail.com>
2017-07-06 10:14:01 +0100

    feat: creates a new create new hero button

--- src/scenes/super-hero-list/app/app.js
+++ src/scenes/super-hero-list/app/app.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
+import PropTypes from "prop-types";
 
 import HeroList from "../super-hero-list";
 
-export default function () {
-  return (
-    <div className="container">
-      <HeroList />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleCreateNewHero = this.handleCreateNewHero.bind(this);
+  }
+
+  handleCreateNewHero() {
+    this.context.router.history.push("/heroes/new");
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <button onClick={this.handleCreateNewHero}>New Hero</button>
+        <HeroList />
+      </div>
+    );
+  }
 }
+
+App.contextTypes = {
+  router: PropTypes.object
+};
+
+export default App;
