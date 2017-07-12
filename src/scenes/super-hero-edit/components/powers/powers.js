74ebbc0d9345f631c1292115635ee43a6da1819e
Adel Jojo <bigfanjs@gmail.com>
2017-07-12 21:30:26 +0100

    feat: makes the powers section in the hero edit better looking

--- src/scenes/super-hero-edit/components/powers/powers.js
+++ src/scenes/super-hero-edit/components/powers/powers.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {Field, FieldArray} from "redux-form";
 import TextField from "material-ui/TextField";
-import RaisedButton from "material-ui/RaisedButton";
 import IconButton from "material-ui/IconButton";
-import ActionHome from "material-ui/svg-icons/action/delete";
+import Delete from "material-ui/svg-icons/action/delete";
+import Add from "material-ui/svg-icons/content/add";
+import RaisedButton from "material-ui/RaisedButton";
 
 import {required, maxLength, minLength} from "../../services/validations";
 
@@ -19,27 +20,30 @@ const Power = ({input, label, meta: { touched, error }, ...custom}) => (
 
 const Powers = function ({fields, meta: { error }}) {
   return (
-    <ul style={{ listStyleType: "none", padding: 0 }}>
+    <ul style={{listStyleType: "none", padding: 0}}>
       <li>
         <RaisedButton
-          label="Add Power"
+          fullWidth={true}
+          backgroundColor="#e6e6e6"
           onTouchTap={() => fields.push()}
+          icon={ <Add /> }
           />
       </li>
       { fields.map((power, index) => (
-        <li key={index}>
+        <li key={index} style={{display: "flex", alignItems: "baaseline"}}>
           <Field
             name={power}
             type="text"
             component={Power}
             label={`Power #${index + 1}`}
             validate={[required, maxLength(30), minLength(3)]}
+            style={{flex: 1}}
             />
           <IconButton
             label="Remove power"
             iconStyle={{color: "#e62828", width: "30px", height: "30px"}}
             onTouchTap={() => fields.remove(index)}>
-              <ActionHome />
+              <Delete />
             </IconButton>
         </li>
       ))}
